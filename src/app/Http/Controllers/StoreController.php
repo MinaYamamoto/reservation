73f26e39687f1cac302d165214f9a169c65c24eb
mina yamamoto <motitama1106@gmail.com>
2024-03-12 20:19:40 +0900

    S3画像保存用のロジック

--- src/app/Http/Controllers/StoreController.php
+++ src/app/Http/Controllers/StoreController.php
@@ -70,8 +70,8 @@ class StoreController extends Controller
             if(app()->isLocal()) {
                 $upStore['thumbnail'] = Storage::disk('local')->putFileAs('public/post_img', $request->file('thumbnail'), $file_name);
             } else {
-                dd($request->file('thumbnail'));
-                $path = Storage::disk('s3')->put('/', $request->file('thumbnail'), 'public');
+                $path = Storage::disk('s3')->put('/', $request->file('thumbnail'), $file_name);
+                dd($file_name);
                 $upStore['thumbnail'] = Storage::disk('s3')->url($path);
             }
         }
