783aa7e8993bc85aaa419d0fa7aff67d95384e14
mina yamamoto <motitama1106@gmail.com>
2024-07-22 14:01:32 +0900

    初期投入データ修正 インデント修正 フォントサイズ変更

--- src/database/seeders/ReviewsTableSeeder.php
+++ src/database/seeders/ReviewsTableSeeder.php
@@ -17,9 +17,9 @@ class ReviewsTableSeeder extends Seeder
         $param = [
             'user_id' => 3,
             'store_id' => 1,
-            'star' => 3,
-            'comment' => 'テスト用レビューの作成',
-            'image'=>'',
+            'star' => 4,
+            'comment' => 'コースでいい値段ですが、お店の雰囲気はフォーマルな感じではなく品があっていい雰囲気です。予約して行きましたが、テーブルに感謝のメッセージが書かれてありました。',
+            'image'=>'/storage/post_img/yakiniku.jpeg',
             'created_at' => '2024-7-19',
             'updated_at' => '2024-7-19'
         ];
@@ -27,41 +27,81 @@ class ReviewsTableSeeder extends Seeder
         $param = [
             'user_id' => 4,
             'store_id' => 1,
-            'star' => 5,
-            'comment' => 'テスト用レビューの作成',
+            'star' => 1,
+            'comment' => 'テスト用レビューの作成 user1',
             'image'=>'',
             'created_at' => '2024-7-20',
             'updated_at' => '2024-7-20'
         ];
         DB::table('reviews')->insert($param);
         $param = [
+            'user_id' => 5,
+            'store_id' => 1,
+            'star' => 3,
+            'comment' => 'テスト用レビューの作成 user2',
+            'image'=>'/storage/post_img/izakaya.jpeg',
+            'created_at' => '2024-7-19',
+            'updated_at' => '2024-7-19'
+        ];
+        DB::table('reviews')->insert($param);
+        $param = [
             'user_id' => 3,
             'store_id' => 2,
             'star' => 1,
-            'comment' => 'テスト用レビューの作成',
-            'image'=>'',
+            'comment' => 'テスト用レビューの作成 user',
+            'image'=>'/storage/post_img/izakaya.jpeg',
             'created_at' => '2024-7-21',
             'updated_at' => '2024-7-21'
+        ];
+        DB::table('reviews')->insert($param);
+        $param = [
+            'user_id' => 4,
+            'store_id' => 2,
+            'star' => 5,
+            'comment' => 'テスト用レビューの作成 user1',
+            'image'=>'/storage/post_img/yakiniku.jpeg',
+            'created_at' => '2024-7-20',
+            'updated_at' => '2024-7-20'
+        ];
+        DB::table('reviews')->insert($param);
+        $param = [
+            'user_id' => 5,
+            'store_id' => 2,
+            'star' => 3,
+            'comment' => 'テスト用レビューの作成 user2',
+            'image'=>'/storage/post_img/izakaya.jpeg',
+            'created_at' => '2024-7-21',
+            'updated_at' => '2024-7-21'
+        ];
+        DB::table('reviews')->insert($param);
+        $param = [
+            'user_id' => 3,
+            'store_id' => 5,
+            'star' => 5,
+            'comment' => 'テスト用のレビュー作成 user',
+            'image'=>'/storage/post_img/yakiniku.jpeg',
+            'created_at' => '2024-7-19',
+            'updated_at' => '2024-7-19'
+        ];
+        DB::table('reviews')->insert($param);
+        $param = [
+            'user_id' => 4,
+            'store_id' => 5,
+            'star' => 4,
+            'comment' => 'テスト用レビューの作成 user1',
+            'image'=>'/storage/post_img/yakiniku.jpeg',
+            'created_at' => '2024-7-20',
+            'updated_at' => '2024-7-20'
         ];
         DB::table('reviews')->insert($param);
         $param = [
             'user_id' => 5,
             'store_id' => 5,
-            'star' => 3,
-            'comment' => 'テスト用レビューの作成',
-            'image'=>'',
-            'created_at' => '2024-7-21',
-            'updated_at' => '2024-7-21'
-        ];
-        DB::table('reviews')->insert($param);
-        $param = [
-            'user_id' => 5,
-            'store_id' => 1,
-            'star' => 4,
-            'comment' => 'テスト用レビューの作成',
-            'image'=>'',
-            'created_at' => '2024-7-19',
-            'updated_at' => '2024-7-19'
+            'star' => 5,
+            'comment' => 'テスト用レビューの作成 user2',
+            'image'=>'/storage/post_img/izakaya.jpeg',
+            'created_at' => '2024-7-22',
+            'updated_at' => '2024-7-22'
         ];
         DB::table('reviews')->insert($param);
     }
--- src/resources/views/review/list.blade.php
+++ src/resources/views/review/list.blade.php
@@ -25,90 +25,93 @@
             </div>
         </form>
         @if(!is_Null($reviews))
-        @foreach($reviews as $review)
-        <hr>
-        <div class="review__show" data-review-id="{{ $review->id }}">
-            <div class="review__flex">
-            @if ($review->user_id === optional(auth()->user())->id)
-                <div class="review-edit__button">
-                    <button class="review-correction__edit-button">口コミを編集</button>
-                </div>
-            @endif
-            @if ($review->user_id === optional(auth()->user())->id || optional(auth()->user())->role === 'admin')
-                <form class="review-delete__form" action="/review/{{ $review->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <div class="review-delete__button">
-                        <button type="submit" class="review-correction__delete-button">口コミを削除</button>
-                        <input type="hidden" name="id" value="{{ $review->id }}">
-                        <input type="hidden" name="store_id" value="{{ $store->id }}">
+            @foreach($reviews as $review)
+            <hr>
+            <div class="review__show" data-review-id="{{ $review->id }}">
+                <div class="review__flex">
+                @if ($review->user_id === optional(auth()->user())->id)
+                    <div class="review-edit__button">
+                        <button class="review-correction__edit-button">口コミを編集</button>
                     </div>
-                </form>
-            @endif
-            </div>
-            <form class="review-edit__form" action="/review/{{ $review->id }}" method="post" data-review-id="{{ $review->id }}" style="display: {{ $errors->has('comment') && old('id') == $review->id ? 'block' : 'none' }};" enctype="multipart/form-data">
+                @endif
+                @if ($review->user_id === optional(auth()->user())->id || optional(auth()->user())->role === 'admin')
+                    <form class="review-delete__form" action="/review/{{ $review->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <div class="review-delete__button">
+                            <button type="submit" class="review-correction__delete-button">口コミを削除</button>
+                            <input type="hidden" name="id" value="{{ $review->id }}">
+                            <input type="hidden" name="store_id" value="{{ $store->id }}">
+                        </div>
+                    </form>
+                @endif
+                </div>
+                <form class="review-edit__form" action="/review/{{ $review->id }}" method="post" data-review-id="{{ $review->id }}" style="display: {{ $errors->has('comment') && old('id') == $review->id ? 'block' : 'none' }};" enctype="multipart/form-data">
                 @csrf
-                @method('patch')
-                <div class="review__star">
-                    @for($i = 1; $i <= 5; $i++)
-                    <label class="star-rating {{ $i <= $review->star ? 'star-rating' : 'star-no-rating' }}" data-rating="{{ $i }}">★</label>
-                    @endfor
-                    <input type="hidden" name="star" value="{{ $review->star }}" class="star-input">
-                    <div class="form__review-error">
-                        @error('star')
-                        {{ $message }}
-                        @enderror
+                    @method('patch')
+                    <div class="review__star">
+                        @for($i = 1; $i <= 5; $i++)
+                        <label class="star-rating {{ $i <= $review->star ? 'star-rating' : 'star-no-rating' }}" data-rating="{{ $i }}">★</label>
+                        @endfor
+                        <input type="hidden" name="star" value="{{ $review->star }}" class="star-input">
+                        <div class="form__review-error">
+                            @error('star')
+                            {{ $message }}
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="review__comment">
-                    <textarea id="textarea" class="review__comment" name="comment" rows="5" cols="60" maxlength="400">{{ $review->comment }}</textarea>
-                    <div class="input__length">
-                        <p class="length">0</p>
-                        <p>/400(最高文字数)</p>
-                    </div>
-                    <div class="form__review-error">
-                        @error('comment')
-                        {{ $message }}
-                        @enderror
-                    </div>
-                    <div class="image__preview" id="imagePreview">
-                        <img id="reviewImage" class="review__img" src="{{ ($review->image) }}"></img>
-                    </div>
-                    <div class="review__image" id="review__image">
+                    <div class="review__comment">
+                        <textarea id="textarea" class="review__comment" name="comment" rows="5" cols="60" maxlength="400">{{ $review->comment }}</textarea>
+                        <div class="input__length">
+                            <p class="length">0</p>
+                            <p>/400(最高文字数)</p>
+                        </div>
+                        <div class="form__review-error">
+                            @error('comment')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                        <div class="image__preview" id="imagePreview">
+                            <img id="reviewImage" class="review__img" src="{{ ($review->image) }}"></img>
+                        </div>
+                        <div class="review__image" id="review__image">
                             <label for="file-input" class="review__file-input">クリックして写真を追加</br>またはドラックアンドドロップ</label>
                             <input type="file" id ="file-input" class="review__file" value="{{ Session::get('image') }}" name="image"/>
+                        </div>
+                        <div class="form__review-error">
+                            @error('image')
+                            {{ $message }}
+                            @enderror
+                        </div>
+                        <input type="hidden" name="id" value="{{ $review->id }}">
+                        <input type="hidden" value="{{ $store->id }}" name="store_id">
                     </div>
-                    <div class="form__review-error">
-                        @error('image')
-                        {{ $message }}
-                        @enderror
+                    <div class="review-save">
+                        <button type="submit" class="review-save__button">保存</button>
                     </div>
-                    <input type="hidden" name="id" value="{{ $review->id }}">
-                    <input type="hidden" value="{{ $store->id }}" name="store_id">
-                </div>
-                <div class="review-save">
-                    <button type="submit" class="review-save__button">保存</button>
-                </div>
-            </form>
-            <div class="review-display"  data-review-id="{{ $review->id }}" style="display: {{ $errors->has('comment') && old('id') == $review->id ? 'none' : 'block' }};">
-                <div class="review__star">
-                    @for($i = 1; $i <= 5; $i++)
-                        @if($i <= $review->star)
-                            <label class="star-rating">★</label>
-                        @else
-                            <label class="star-no-rating">★</label>
-                        @endif
-                    @endfor
-                </div>
-                <div class="review__comment">
-                    <p class="review__comment-txt">{{ $review->comment }}</p>
+                </form>
+                <div class="review-display"  data-review-id="{{ $review->id }}" style="display: {{ $errors->has('comment') && old('id') == $review->id ? 'none' : 'block' }};">
+                    <div class="review__star">
+                        @for($i = 1; $i <= 5; $i++)
+                            @if($i <= $review->star)
+                                <label class="star-rating">★</label>
+                            @else
+                                <label class="star-no-rating">★</label>
+                            @endif
+                        @endfor
+                    </div>
+                    <div class="review__comment">
+                        <p class="review__comment-txt">{{ $review->comment }}</p>
+                    </div>
+                    <div class="review-display__image">
+                        <img class="review__img" src="{{ ($review->image) }}"></img>
+                    </div>
                 </div>
             </div>
-        </div>
-        @if ($loop->last)
-            <hr>
-        @endif
-        @endforeach
+            @if ($loop->last)
+                <hr>
+            @endif
+            @endforeach
         @endif
     </div>
     <script src="{{ mix('js/reviewlist.js')}}"></script>
